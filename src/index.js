e4e58ee9206fb763e1273955fa312f72d784f68b
semen <i@schanel.ru>
2025-01-15 21:20:38 +0400

    new_swiper

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-import './styles.scss';
+import './styles.scss'; // испортируем сюда стили
 
 import 'normalize.css'; // Импортируем normalize.css
+
+//логика первого свайпера тест
+import './swiper.js'
+
+//логика второго свайпера тест
+
+//логика третьего свайпера тест
+
+//кидаем сюда те картинки, которые не подкючились нормально
 import exampleImage from './image/image.jpg';
+import exampleEmagev2 from './image/expand.svg';
+import exampleEmagev3 from './image/Group.svg';
+import exampleEmagev4 from './image/divider.svg';
 
-// const img = document.createElement('img');
-// img.src = exampleImage;
-// document.body.appendChild(img)
 
-console.log('Привет вебпак')
+console.log('Привет вебпак') // простой вывод в консоль
---
+++ src/swiper.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let swiperInstance;
+
+    const initSwiper = () => {
+        if (window.innerWidth <= 768 && !swiperInstance) { // Проверяем, мобильная ли версия
+            swiperInstance = new Swiper('.swiper', {
+                slidesPerView: 1.23,
+                spaceBetween: 10,
+                loop: true,
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true,
+                },
+            });
+        } else if (window.innerWidth > 768 && swiperInstance) { // Отключаем Swiper на большом экране
+            swiperInstance.destroy(true, true);
+            swiperInstance = null;
+        }
+    };
+
+
+
+    // Инициализация при загрузке страницы
+    initSwiper();
+
+    // Отслеживаем изменение размера окна
+    window.addEventListener('resize', initSwiper);
+});
