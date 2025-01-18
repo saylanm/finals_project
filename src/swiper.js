d07a4adcc862d9a1e1babf5bc97e67b7b2951a2c
semen <i@schanel.ru>
2025-01-18 18:05:29 +0400

    gotovo_2swiper

--- src/swiper.js
+++ src/swiper.js
@@ -63,28 +63,19 @@ var vidimo = document.querySelector('.modal2');
 var action_buttons = document.querySelector('.action-buttons'); 
 var action_buttons2 = document.querySelector('.action-buttons2');
 
-// Проверяем состояние при загрузке страницы
-window.addEventListener('DOMContentLoaded', function () {
-    action_buttons2.classList.add('modal2'); // Скрываем "Скрыть" при загрузке
-    action_buttons.classList.remove('modal2'); // Показываем "Показать все"
-});
-
 // Логика "Показать все"
 opens_button.addEventListener('click', function (evt) {
     evt.preventDefault();
     console.log('Показать все: work');
-
-    // Показать скрытые элементы
+    action_buttons.classList.add('modal2'); // Скрываем "Показать все"
     vidimo.classList.add('modal3'); // Показываем содержимое
     action_buttons2.classList.remove('modal2'); // Показываем "Скрыть"
-    action_buttons.classList.add('modal2'); // Скрываем "Показать все"
+   
 });
 
 // Логика "Скрыть"
 hide_button.addEventListener('click', function () {
     console.log('Скрыть: work');
-
-    // Скрыть элементы
     vidimo.classList.remove('modal3'); // Скрываем содержимое
     action_buttons2.classList.add('modal2'); // Скрываем "Скрыть"
     action_buttons.classList.remove('modal2'); // Показываем "Показать все"
