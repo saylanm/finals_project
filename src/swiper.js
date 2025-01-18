50bc050e2605d2dca663a4c47f22a4f6ec05698e
semen <i@schanel.ru>
2025-01-18 11:44:28 +0400

    new_768_icons-1

--- src/swiper.js
+++ src/swiper.js
@@ -28,3 +28,30 @@ document.addEventListener('DOMContentLoaded', () => {
     // Отслеживаем изменение размера окна
     window.addEventListener('resize', initSwiper);
 });
+
+var button = document.querySelector('.open');
+var button_closed = document.querySelector('.closesd');
+var close = document.querySelector('.close');
+var content = document.querySelector('.modal');
+var img = document.querySelector('.close');
+var image = document.querySelector('.img');
+
+button.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    content.classList.add('modal-show');
+    button.classList.add('modal');
+    img.classList.add('modal');
+    button_closed.classList.remove('modal');
+    img.classList.remove('modal');
+    image.classList.add('modal');
+
+})
+
+button_closed.addEventListener('click', function() {
+    content.classList.remove('modal-show');
+    button.classList.remove('modal');
+    img.classList.remove('modal');
+    button_closed.classList.add('modal');
+    image.classList.remove('modal');
+    close.classList.add('modal');
+})
